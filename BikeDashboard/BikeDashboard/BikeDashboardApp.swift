9a9fdfb6e0d282e010763b314c1421b198a10272
mgranic <mgranic11@gmail.com>
2023-12-04 20:02:31 +0100

    Initial commit
    just created the empty SwiftUI app

---
+++ BikeDashboard/BikeDashboard/BikeDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeDashboardApp.swift
+//  BikeDashboard
+//
+//  Created by Mate Granic on 04.12.2023..
+//
+
+import SwiftUI
+
+@main
+struct BikeDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
