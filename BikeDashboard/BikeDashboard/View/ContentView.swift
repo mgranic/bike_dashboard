6b8796ff8c4fe2423620de2b30885063eab611d5
mgranic <mgranic11@gmail.com>
2023-12-22 17:45:08 +0100

    Spacer as a percentage
    Spacer size is now percentage of screen height

--- BikeDashboard/BikeDashboard/View/ContentView.swift
+++ BikeDashboard/BikeDashboard/View/ContentView.swift
@@ -16,12 +16,12 @@ struct ContentView: View {
             VStack {
                 ZStack {
                     VStack {
-                        Spacer(minLength: 50)
+                        Spacer(minLength: UIScreen.main.bounds.height * 0.05)
                         SpeedometerGauge(startAngleDegrees: -180, endAngleDegrees: 0)
                             .stroke(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/, lineWidth: 30)
                     }
                     VStack {
-                        Spacer(minLength: 50)
+                        Spacer(minLength: UIScreen.main.bounds.height * 0.05)
                         SpeedometerGauge(startAngleDegrees: 0, endAngleDegrees: locationManager.currentSpeed)
                             .rotation(Angle(degrees: 180))
                             .stroke(Color.red, lineWidth: 20)
