62ce53be45d43d1af95c49b808fbbd71abf3e220
mgranic <mgranic11@gmail.com>
2024-05-28 12:51:48 +0200

    Refactor
    - change name of content view file to MainScreen
    - group files into folders
    - put speedometer gauge part of main svcreen into separate file and use it like that

---
+++ BikeDashboard/BikeDashboard/View/screens/MainScreen.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  BikeDashboard
+//
+//  Created by Mate Granic on 04.12.2023..
+//
+
+import SwiftUI
+import MapKit
+
+struct MainScreen: View {
+    @Environment(\.scenePhase) private var scenePhase
+    @StateObject var locationManager = LocationManager()
+    var body: some View {
+        VStack {
+            VStack {
+                SpeedometerDashboard()
+                    .environmentObject(locationManager)
+            Map(coordinateRegion: $locationManager.mapRegion, showsUserLocation: true,
+                userTrackingMode: .constant(.follow))
+            }
+            .onAppear {
+                locationManager.startLocationMonitoring()
+                UIApplication.shared.isIdleTimerDisabled = true
+            }
+        }
+        .onChange(of: scenePhase, {
+            if (scenePhase == .background) {
+                // store total distance in UserDefauls
+                locationManager.saveTotalDistance()
+            } else if (scenePhase == .active) {
+                // read data distance from userDefaults
+                locationManager.loadTotalDistance()
+            }
+        })
+        .padding()
+    }
+}
+
+#Preview {
+    MainScreen()
+}
---
+++ BikeDashboard/BikeDashboard/View/widgets/SpeedometerDashboard.swift
@@ -0,0 +1,47 @@
+//
+//  SpeedometerDashboard.swift
+//  BikeDashboard
+//
+//  Created by Mate Granic on 28.05.2024..
+//
+
+import SwiftUI
+
+struct SpeedometerDashboard: View {
+    @EnvironmentObject var locationManager: LocationManager
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer(minLength: UIScreen.main.bounds.height * 0.05)
+                SpeedometerGauge(startAngleDegrees: -180, endAngleDegrees: 0)
+                    .stroke(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/, lineWidth: 30)
+            }
+            VStack {
+                Spacer(minLength: UIScreen.main.bounds.height * 0.05)
+                SpeedometerGauge(startAngleDegrees: 0, endAngleDegrees: locationManager.currentSpeed)
+                    .rotation(Angle(degrees: 180))
+                    .stroke(Color.red, lineWidth: 20)
+            }
+            VStack {
+                Text("\(locationManager.currentSpeed, specifier: "%.1f") km/h")
+                    .font(.largeTitle)
+                HStack {
+                    Text("Odometer:")
+                    Text("\(locationManager.totalDistance, specifier: "%.2f") km")
+                }
+                HStack {
+                    Text("Trip")
+                    Text("\(locationManager.tripDistance, specifier: "%.2f") km")
+                }
+                Button("Reset trip") {
+                    locationManager.resetTrip()
+                }
+                .buttonStyle(.borderedProminent)
+            }
+        }
+    }
+}
+
+#Preview {
+    SpeedometerDashboard()
+}
